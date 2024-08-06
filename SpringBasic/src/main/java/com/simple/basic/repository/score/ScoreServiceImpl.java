f5a0806267c2a1342fc09199a0888367789fda68
ddd654 <bound654@naver.com>
2024-08-06 11:33:46 +0900

    AOP 로그 만들기

--- SpringBasic/src/main/java/com/simple/basic/repository/score/ScoreServiceImpl.java
+++ SpringBasic/src/main/java/com/simple/basic/repository/score/ScoreServiceImpl.java
@@ -2,16 +2,22 @@ package com.simple.basic.repository.score;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.simple.basic.command.ScoreVO;
-import com.simple.basic.service.score.impl.ScoreDAO;
+import com.simple.basic.util.aop.LogAdvice;
 
 @Service("co") // 반드시 component-scan에 의하여 읽힌다
 public class ScoreServiceImpl implements ScoreService {
 
+	
+	//public static final Logger log = LoggerFactory.getLogger(ScoreServiceImpl.class);
+
+	
 //	//SQL 세션 팩토리 및 마이바티스 후 필요 없어서 > 이제부터 ScoreMapper 만들고 기능 추가
 //	// 15. 맨아래 퍼블릭 주석하고 실행해줌
 //	@Autowired
@@ -30,7 +36,7 @@ public class ScoreServiceImpl implements ScoreService {
 		//scoreDAO.regist(vo);
 		scoreMapper.regist(vo);
 		
-		System.out.println("AOP클래스 보기용 score메서드 동작~~1");
+		System.out.println("AOP클래스 보기용 regist메서드 동작");
 	}
 
 //	public void regist(ScoreVO vo) {
@@ -43,7 +49,7 @@ public class ScoreServiceImpl implements ScoreService {
 	// 20. >컨
 	public ArrayList<ScoreVO> getScore(){
 		//return scoreDAO.getScore();
-		System.out.println("AOP클래스 보기용 score메서드 동작~~2");
+		System.out.println("AOP클래스 보기용 getScore메서드 동작");
 
 //		//에러용 코드
 //		String str=null;
@@ -55,15 +61,19 @@ public class ScoreServiceImpl implements ScoreService {
 		
 		}
 
+	//before 메서드 실행부분
+	
 	// 8) DAO 와 DAO임플도 똑같이
 	@Override
 	public void delete(int sno) {
 		// TODO Auto-generated method stub
-		System.out.println("AOP클래스 보기용 score메서드 동작~~3");
+		System.out.println("AOP클래스 보기용 delete메서드 동작");
 
 		//12) > 과정은 DAO impl을 거치고 > 컨
 		//scoreDAO.delete(sno);
 		scoreMapper.delete(sno);
 	}
+	
+	//after 메서드 실행 부분
 
 }
--- SpringBasic/src/main/java/com/simple/basic/util/aop/LogAdvice.java
+++ SpringBasic/src/main/java/com/simple/basic/util/aop/LogAdvice.java
@@ -1,38 +1,47 @@
 package com.simple.basic.util.aop;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Arrays;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect // AOP클래스
 @Component // 이 클래스는 component-scan에 읽혀서 bean으로 생성된다
 public class LogAdvice {
 
+	// 2_ 해당클래스에서 로그를 사용함 - base 패키지 밑에 있어야한다
+	public static final Logger log = LoggerFactory.getLogger(LogAdvice.class);
+
 	// advice - 공통기능
 	// point cut - 내가 적용할 메서드에 advice를 부착하는 범위
 
 	// 맨압 *은 모든 접근제어자, 마지막 *(..)은 모든 메서드에
 	// @Before("execution(*com.simple.basic.controller.*Controller.*(..))")
-	// @Before("execution(*com.simple.basic.service.score.*ScoreServiceImpl.*(..))")
+	//@Before("execution(* com.simple.basic.repository.score.*ScoreServiceImpl.*(..))")
 	public void beforeLog() {
 		// 위에 경로 before 를 지나면 동작되는곳
 		System.out.println("-----메서드 실행전");
 	}
 
-	// @After("execution(*com.simple.basic.service.score.*ScoreServiceImpl.*(..))")
+	//@After("execution(* com.simple.basic.repository.score.*ScoreServiceImpl.*(..))")
 	public void afterLog() {
 		System.out.println("-----메서드 실행하고 나서");
 
 	}
 
-	// @AfterThrowing( pointcut =
-	// "execution(*com.simple.basic.service.score.*ScoreServiceImpl.*(..))",
-	// throwing = "e" //에러를 처리할 변수명
-	// )
+//	@AfterThrowing( pointcut =
+//	"execution(*com.simple.basic.service.score.*ScoreServiceImpl.*(..))",
+//	throwing = "e" //에러를 처리할 변수명
+//	)
 	public void exceptionLog(Exception e) {
 
 		System.out.println("coco advice 에러로그: " + e);
@@ -40,26 +49,55 @@ public class LogAdvice {
 	}
 
 	//
-	// 이것들 전부 한번에 사용하는게 AROUND 메서드
+	// 이것들 전부 한번에 사용하는게 AROUND 메서드 < 핵심
 
 	// AROUND
 	// 리턴을 오브젝트로 만들고, 매개변수로 실행 시점을 결정한다
+	// 아랫줄 주석하면 실행막아짐
 	
-	
-	//아랫줄 주석하면 실행막아짐
-	
-	
-	@Around("execution(*com.simple.basic.service.score.*ScoreServiceImpl.*(..))")
+	@Around("* com.simple.basic.repository.score.*ScoreServiceImpl.*(..))")
 	public Object aroundLog(ProceedingJoinPoint pj) {
 
-		// 여기 비어두면 서비스 실행 안됨
+		// 여기 비워두면 서비스 실행 안됨
 
+		//1_ 08-06 매개변수를 확인해보자
+		System.out.println("실행클래스:  " + pj.getTarget());
+		System.out.println("실행메서드:  " + pj.getSignature().toString());
+		System.out.println("매개변수값:  " + Arrays.toString(pj.getArgs()));
+		
+		
+		//5_ 실행시간 알아보기1
+		long start = System.currentTimeMillis();
+		
+		//3_  > 로그를 임플에 붙여서 쓸 수도 있다 ex. 스코어 임플
+		log.info("실행클래스22:  " + pj.getTarget());
+		
 		Object result = null;
 
 		try {
 
+			// before
 			result = pj.proceed(); // proceed 메서드가 실행되면, 타겟 메서드가 실행
+			// after
 
+			//6_ 실행시간 알아보기2
+			long end = System.currentTimeMillis();
+			
+			log.info("실행시간 알아보기  " + (end -start) * 0.001);
+			
+			//7_파일형태로 로그 남기기 "파일 내릴 경로"
+			
+			BufferedWriter bos = new BufferedWriter(new FileWriter("C:\\Users\\user\\Desktop\\course\\spring\\Spring\\way\\오늘날짜_Log.txt", true));
+
+			bos.write("실행클래스22:  " + pj.getTarget() + "\n");
+			bos.write("실행메서드:  " + pj.getSignature().toString());
+			bos.write("매개변수값:  " + Arrays.toString(pj.getArgs()));
+			bos.write("실행시간 알아보기  " + (end -start) * 0.001);
+			bos.flush();
+			
+			bos.close();
+			
+			
 		} catch (Throwable e) {
 			// TODO: handle exception
 			e.printStackTrace(); // 해당 메서드의 에러찍기
